9d5358fca1961a66f69ff022ab5a7afd55f5564a
asabou <alexmihai906@gmail.com>
2023-11-16 12:51:13 +0200

    add small test

--- src/test/java/com/rurbisservices/springreactive/weather/WeatherApplicationTests.java
+++ src/test/java/com/rurbisservices/springreactive/weather/WeatherApplicationTests.java
@@ -1,13 +1,34 @@
 package com.rurbisservices.springreactive.weather;
 
+import com.rurbisservices.springreactive.weather.service.impl.WeatherServiceImpl;
+import com.rurbisservices.springreactive.weather.service.model.TemperatureDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.anyString;
+
+@WebFluxTest
 class WeatherApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@MockBean
+	private WeatherServiceImpl weatherService;
 
 	@Test
-	void contextLoads() {
+	public void getTemperaturesTest() {
+		Flux<TemperatureDTO> tempFlux = Flux.just(new TemperatureDTO("city1", "1", "1"),
+				new TemperatureDTO("city2", "2", "2"));
+		Mockito.when(weatherService.getTemperatureV2(anyString())).thenReturn(tempFlux);
+		webTestClient.get().uri("/api/weather?city=city1,city2")
+				.exchange()
+				.expectStatus().isOk();
+		Mockito.verify(weatherService, Mockito.times(1)).getTemperatureV2(anyString());
 	}
 
 }
